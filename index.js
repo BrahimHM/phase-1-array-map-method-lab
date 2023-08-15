e48f3abdd5329011591b576b16a3931f7a08ef57
Brahim HM <brahimhm98@gmail.com>
2023-08-15 20:59:06 +0100

    completed

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const words= tutorials.map((item)=>{
+    return item.split(' ').map((word)=>{
+    return word.charAt(0).toUpperCase() + word.slice(1);  
+    })
+  })
+  return words.map(element=>{
+   return  element.join(' ')
+  })
 }
